85b3426ce2ecb98c2650ea6554744ad20b0cda5a
F-Torres15 <fx109090@gmail.com>
2024-11-26 21:23:50 -0300

    Add comentários

--- script.js
+++ script.js
@@ -1,51 +1,39 @@
 
+// Cria uma nova instância do objeto XMLHttpRequest para fazer requisições HTTP
 const objt = new XMLHttpRequest();
-const corpo= document.querySelector("body")
-const tec = corpo.querySelector(".tec")
+// Seleciona o corpo da página (body) e um elemento com a classe "tec" dentro do corpo
+const corpo = document.querySelector("body");
+const tec = corpo.querySelector(".tec");
+// Inicia uma requisição GET para buscar o arquivo JSON da URL especificada
 objt.open("GET", "https://raw.githubusercontent.com/DS-Senai/json/refs/heads/main/tecnologias.json");
+// Define que a resposta será interpretada como JSON
 objt.responseType = "json";
+// Envia a requisição
 objt.send();
-
-objt.onload = () =>{
+// Define o que deve ser feito quando a requisição for concluída
+objt.onload = () => {
+    // A resposta da requisição (JSON) é armazenada na variável 'tecnologia'
     let tecnologia = objt.response;
-
-tecnologia.tecnologias. forEach((valor) => {
-
-    for(let chave in valor){
-if(chave != "bibliotecas_populares"){
-    
-    tec.innerHTML+=
-    `<p>${chave}: ${valor[chave]}</p>`
-     }
-     else{
-
-        valor.bibliotecas_populares.forEach((biblioteca)=>{
-            for(let chave_biblioteca in biblioteca){
-                tec.innerHTML+=`<ul>${chave_biblioteca}: ${biblioteca[chave_biblioteca]}</ul>`
+    // Acessa o array de tecnologias dentro do objeto 'tecnologia'
+    tecnologia.tecnologias.forEach((valor) => {
+        // Para cada item dentro do array de tecnologias, percorre suas chaves (propriedades)
+        for (let chave in valor) { 
+        // Se a chave não for "bibliotecas_populares", adiciona o conteúdo ao HTML
+            if (chave != "bibliotecas_populares") {
+                tec.innerHTML +=
+                    `<p>${chave}: ${valor[chave]}</p>`;
+            } else {
+                // Caso contrário, se a chave for "bibliotecas_populares", percorre o array de bibliotecas
+                valor.bibliotecas_populares.forEach((biblioteca) => {   
+                    // Para cada biblioteca, percorre suas chaves e valores e os adiciona ao HTML
+                    for (let chave_biblioteca in biblioteca) {
+                        tec.innerHTML +=
+                            `<ul>${chave_biblioteca}: ${biblioteca[chave_biblioteca]}</ul>`;
+                    }
+                });
             }
-        })
-     
-    }
-    } 
-    tec.innerHTML+="<br>"
-
-})
-
-
-
-}
-
-
-// bibioteca.forEach((tecnologias.bibliotecas_polpulares)){
-//     if (bibioteca.)
-// }
-
-
-
-
-// // forEach() percorre os valores de um array.
-// var obj = [{Nome: "João", Idade: 20 },{Nome: "Lucas", Idade: 19 }]; // Só pode ser utilizado em arrays 
-
-// obj.forEach((objeto) => {
-//   console.log(objeto.Nome + " " + objeto.Idade); // Exemplo: João 20 | Lucas 19
-// })
+        }
+        // Adiciona uma quebra de linha após cada tecnologia para separá-las visualmente
+        tec.innerHTML += "<br>";
+    });
+};
